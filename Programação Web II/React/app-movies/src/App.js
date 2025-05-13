76d38192f2bcc9284dff241d4b3bf5731e22672a
ADS 2023.1N <36129382023.1N@senacrjedu.local>
2025-05-13 20:18:25 -0300

    Modulo 5

---
+++ Programação Web II/React/app-movies/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+    return (
+        <>
+            <Header/>
+            <h1> Meu ReactFlix</h1>
+            <p>Testando</p>
+            <Footer/>
+        </>
+    );
+}
+
+export default App;
---
+++ Programação Web II/React/app-movies/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css"
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2> ReactFlix &copy; Desenvolvido por Erick Calazães</h2>
+        </footer>
+    )
+}
+
+export default Footer;
