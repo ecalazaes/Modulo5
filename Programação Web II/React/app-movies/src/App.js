c616fdbaeb2cb20c9e7735067ce4a14af61694d5
ADS 2023.1N <36129382023.1N@senacrjedu.local>
2025-05-13 20:30:09 -0300

    Modulo 5

--- Programação Web II/React/app-movies/src/App.js
+++ Programação Web II/React/app-movies/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Banner from "./components/Banner/Banner";
 
 function App() {
     return (
         <>
             <Header/>
+            <Banner/>
             <h1> Meu ReactFlix</h1>
             <p>Testando</p>
             <Footer/>
---
+++ Programação Web II/React/app-movies/src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import styles from "./Banner.module.css";
+
+function Banner() {
+    return (
+        <div className={styles.banner}>
+            <span>Meu Banner</span>
+        </div>
+    );
+}
+
+export default Banner;
